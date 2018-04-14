1fda7c0af1cd389a6fbf402f7f4fc179c276aa5f
Alexander Tash <atash@ascendcohealth.com>
2018-04-14 17:31:18 -0500

    demographics

--- app/src/features/clients/TabDemographics.js
+++ app/src/features/clients/TabDemographics.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import { FormGroup, Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
 import * as actions from './redux/actions';
 
@@ -10,10 +11,67 @@ export class TabDemographics extends Component {
     actions: PropTypes.object.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+
+    this.handleChangeEvent = this.handleChangeEvent.bind(this);
+  }
+
+  handleChangeEvent(e) {
+    const { name, value } = e.target;
+    this.props.actions.updateLocalClientInfo(name, value);
+  }
+
   render() {
+    const { clientInfo } = this.props.clients;
     return (
-      <div className="clients-tab-demographics">
-        Page Content: clients/TabDemographics
+      <div className='clients-tab-demographics mt-4'>
+        <div className='row'>
+          <div className='col'>
+            <Label for='gender_cd'>Gender</Label>
+            <Input type='select' name='gender_cd' id='gender_cd' value={clientInfo.gender_cd} onChange={this.handleChangeEvent}>
+              {
+                this.props.clients.GENDER.map(gender => <option key={`gender-option-${gender.id}`} value={gender.id}>{gender.name}</option>)
+              }
+            </Input>
+          </div>
+          <div className='col'>
+            <Label for='ethnicity_cd'>Ethnicity</Label>
+            <Input type='select' name='ethnicity_cd' id='ethnicity_cd' value={clientInfo.ethnicity_cd} onChange={this.handleChangeEvent}>
+              {
+                this.props.clients.ETHNICITY.map(ethnicity => <option key={`ethnicity-option-${ethnicity.id}`} value={ethnicity.id}>{ethnicity.name}</option>)
+              }
+            </Input>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col'>
+            <Label for='race_cd'>Race</Label>
+            <Input type='select' name='race_cd' id='race_cd' value={clientInfo.race_cd} onChange={this.handleChangeEvent}>
+              {
+                this.props.clients.RACE.map(race => <option key={`race-option-${race.id}`} value={race.id}>{race.name}</option>)
+              }
+            </Input>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col'>
+            <Label for='veteran_cd'>Veteran Status</Label>
+            <Input type='select' name='veteran_cd' id='veteran_cd' value={clientInfo.veteran_cd} onChange={this.handleChangeEvent}>
+              {
+                this.props.clients.VETERAN.map(veteran => <option key={`veteran-option-${veteran.id}`} value={veteran.id}>{veteran.name}</option>)
+              }
+            </Input>
+          </div>
+          <div className='col'>
+            <Label for='disability_cd'>Disabling Condition</Label>
+            <Input type='select' name='disability_cd' id='disability_cd' value={clientInfo.disability_cd} onChange={this.handleChangeEvent}>
+              {
+                this.props.clients.DISABILITY.map(disability => <option key={`disability-option-${disability.id}`} value={disability.id}>{disability.name}</option>)
+              }
+            </Input>
+          </div>
+        </div>
       </div>
     );
   }
