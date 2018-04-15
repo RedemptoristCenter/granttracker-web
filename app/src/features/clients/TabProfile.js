c380befc9ec6b88392c83faa6517fef791959cc7
Nathan Brewer-Davis <10ndavis10@gmail.com>
2018-04-15 12:30:18 -0500

    added the red alert to input fields

--- app/src/features/clients/TabProfile.js
+++ app/src/features/clients/TabProfile.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import { FormGroup, Label, Input } from 'reactstrap';
 import moment from 'moment';
 import { connect } from 'react-redux';
+import FormValidator from '../.././Features/Common/FormValidator';
 import * as actions from './redux/actions';
 
 export class TabProfile extends Component {
@@ -35,53 +36,71 @@ export class TabProfile extends Component {
       <div className='clients-tab-profile mt-4'>
         <div className='row'>
           <FormGroup className='col'>
-            <Label for='Lname'>Last Name</Label>
-            <Input type='text' name='Lname' id='Lname' value={clientInfo.Lname || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.Lname || ''}>
+              <Label for='Lname'>Last Name</Label>
+              <Input type='text' name='Lname' id='Lname' value={clientInfo.Lname || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>
           </FormGroup>
           <FormGroup className='col'>
-            <Label for='Fname'>First Name</Label>
-            <Input type='text' name='Fname' id='Fname' value={clientInfo.Fname || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.Fname || ''}>
+              <Label for='Fname'>First Name</Label>
+              <Input type='text' name='Fname' id='Fname' value={clientInfo.Fname || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>
           </FormGroup>
           <FormGroup className='col-2'>
-            <Label for='Mname'>M. Initial</Label>
-            <Input type='text' name='Mname' id='Mname' value={clientInfo.Mname || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.Mname || ''}>
+              <Label for='Mname'>M. Initial</Label>
+              <Input type='text' name='Mname' id='Mname' value={clientInfo.Mname || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>  
           </FormGroup>
           <FormGroup className='col'>
-            <Label for='birth_date'>Date of Birth</Label>
-            <Input
-              type='date'
-              max='2999-12-31'
-              name='birthDateView'
-              id='birthDate'
-              placeholder='Date of Birth'
-              bsSize='sm'
-              value={moment.unix(clientInfo.birth_date).format("YYYY-MM-DD")}
-              onChange={this.handleDateChange}
-            />
+            <FormValidator value={moment.unix(clientInfo.birth_date).format("YYYY-MM-DD")}>
+              <Label for='birth_date'>Date of Birth</Label>
+              <Input
+                type='date'
+                max='2999-12-31'
+                name='birthDateView'
+                id='birthDate'
+                placeholder='Date of Birth'
+                bsSize='sm'
+                value={moment.unix(clientInfo.birth_date).format("YYYY-MM-DD")}
+                onChange={this.handleDateChange}
+              />
+            </FormValidator>
           </FormGroup>
         </div>
         <div className='row mt-3'>
           <FormGroup className='col-6'>
-            <Label for='address'>Street Address</Label>
-            <Input type='text' name='address' id='address' value={clientInfo.address || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.address || ''}>
+              <Label for='address'>Street Address</Label>
+              <Input type='text' name='address' id='address' value={clientInfo.address || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>  
           </FormGroup>
           <FormGroup className='col'>
-            <Label for='city'>City</Label>
-            <Input type='text' name='city' id='city' value={clientInfo.city || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.city || ''}>
+              <Label for='city'>City</Label>
+              <Input type='text' name='city' id='city' value={clientInfo.city || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>
           </FormGroup>
           <FormGroup className='col-2'>
-            <Label for='state'>State</Label>
-            <Input type='text' name='state' id='state' value={clientInfo.state || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.state || ''}>
+              <Label for='state'>State</Label>
+              <Input type='text' name='state' id='state' value={clientInfo.state || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>
           </FormGroup>
           <FormGroup className='col'>
-            <Label for='zipcode'>Zip Code</Label>
-            <Input type='text' name='zipcode' id='zipcode' value={clientInfo.zipcode || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.zipcode || ''}>
+              <Label for='zipcode'>Zip Code</Label>
+              <Input type='text' name='zipcode' id='zipcode' value={clientInfo.zipcode || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>
           </FormGroup>
         </div>
         <div className='row mt-3'>
           <FormGroup className='col-12'>
-            <Label for='phone_num'>Phone Number</Label>
-            <Input type='text' name='phone_num' id='phone_num' value={clientInfo.phone_num || ''} onChange={this.handleChangeEvent} />
+            <FormValidator value={clientInfo.phone_num || ''}>
+              <Label for='phone_num'>Phone Number</Label>
+              <Input type='text' name='phone_num' id='phone_num' value={clientInfo.phone_num || ''} onChange={this.handleChangeEvent} />
+            </FormValidator>
           </FormGroup>
         </div>
       </div>
