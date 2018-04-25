51a5f57313ae737b2133c68b93d8161402458d1d
David Fulton <fultonium@gmail.com>
2018-04-24 21:38:16 -0500

    added credentials flag to axios call

--- app/src/features/clients/redux/requestClientRecords.js
+++ app/src/features/clients/redux/requestClientRecords.js
@@ -22,7 +22,7 @@ export function requestClientRecords(args = {}) {
       // doRequest is a placeholder Promise. You should replace it with your own logic.
       // See the real-word example at:  https://github.com/supnate/rekit/blob/master/src/features/home/redux/fetchRedditReactjsList.js
       // args.error here is only for test coverage purpose.
-      const doRequest = axios.get(`${window.app_config.api_url}/client/${args.client_id}/records`);
+      const doRequest = axios.get(`${window.app_config.api_url}/client/${args.client_id}/records`, { withCredentials: true });
       doRequest.then(
         (res) => {
           dispatch({
